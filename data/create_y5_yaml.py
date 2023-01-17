bf3e4bf6814c8bed3dfc728ad1ea8ad6e75fda8c
pma-bm <pma@businessmann.dk>
2023-01-17 14:24:05 +0100

    more robust yaml creation

--- data/create_y5_yaml.py
+++ data/create_y5_yaml.py
@@ -7,12 +7,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--path2dataset', type=str, default='dataset/', help='path to data directory')
     parser.add_argument('--yolovx', type=str, default='yolov5', help='yolo model to use')
-    parser.add_argument('--labels', type=list, default=None)
+    parser.add_argument('--labels', type=str, default=None)
     parser.add_argument('--yaml_fname', type=str, default='my_data.yaml')
     args = parser.parse_args() 
 
     if args.labels is None:
         sys.exit("No labels passed!")
+
+    labels = args.labels.split(',')
 
     yaml_dict = {
         'path' : args.path2dataset,
