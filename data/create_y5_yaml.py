079e7aacd002b101e4f4de99f69b4538d0010adb
pma-bm <pma@businessmann.dk>
2023-01-17 13:58:54 +0100

    more robust yaml creation

--- data/create_y5_yaml.py
+++ data/create_y5_yaml.py
@@ -4,10 +4,10 @@ import yaml
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path2dataset', type=str, default='/home/orin/vibrator_project/dataset', help='path to data directory')
+    parser.add_argument('--path2dataset', type=str, default='dataset/', help='path to data directory')
     parser.add_argument('--yolovx', type=str, default='yolov5', help='yolo model to use')
-    parser.add_argument('--label', type=str, default='platform')
-    parser.add_argument('--yaml_fname', type=str, default='my_data_platform.yaml')
+    parser.add_argument('--labels', type=list, default=None)
+    parser.add_argument('--yaml_fname', type=str, default='my_data.yaml')
     args = parser.parse_args() 
 
     yaml_dict = {
@@ -15,8 +15,8 @@ if __name__ == '__main__':
         'train': "images/train",  # os.path.join(args.path2dataset, "images/train"),   # path to the train folder
         'val'  : "images/valid",  # os.path.join(args.path2dataset, "images/valid"),   # path to the valid folder
         'test' : "images/test",   # os.path.join(args.path2dataset, "images/test"),    # path to the test folder
-        'nc'   : 1,
-        'names': {0: args.label}
+        'nc'   : len(args.labels),
+        'names': {i: x for i, x in enumerate(args.labels)}
     }
 
     # path2yaml = args.path2dataset.replace('dataset', args.yolovx)
